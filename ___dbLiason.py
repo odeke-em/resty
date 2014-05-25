eecc9ed905e7aff22ac50cde772de7ddd7b21b73
Emmanuel Odeke <emmanuel.odeke@cmgcanada.ca>
2014-05-24 20:37:17 -0600

    Initial commit

---
+++ ___dbLiason.py
@@ -0,0 +1,76 @@
+#!/usr/bin/python3
+# Author: Emmanuel Odeke <odeke@ualberta.ca>
+
+import os
+import sys
+import json
+import collections
+
+pyVersion = sys.hexversion / (1<<24)
+
+if pyVersion >= 3:
+        import urllib.request as urlReqModule
+        byteFyer = dict(encoding='utf-8')
+else:
+        import urllib2 as urlReqModule
+        byteFyer = dict()
+
+class DbConn:
+    def __init__(self, baseUrl):
+        self.baseUrl = baseUrl
+
+    def __urlRequest(self, method, isGet=False, **getData):
+        fmtdData = json.dumps(getData)
+        reqUrl = self.baseUrl
+        if isGet:
+            reqUrl = self.baseUrl + '/?' + '&'.join(
+                ['{k}={v}'.format(k=k, v=v) for k,v in getData.items()]
+            )
+
+        req = urlReqModule.Request(reqUrl)
+        req.add_header('Content-Type', 'application/json')
+        req.get_method = lambda : method.upper()
+
+        dataOut = dict()
+        statusCode = 500
+        try:
+            uR = urlReqModule.urlopen(req, bytes(fmtdData, **byteFyer))
+        except Exception as e:
+            print(e)
+            dataOut['reason'] = e
+        else:
+            dataOut['value'] = uR.read()
+            statusCode = uR.getcode()
+        finally:
+            dataOut['status_code'] = statusCode
+
+        return dataOut
+
+    def get(self, **data):
+        return self.__urlRequest('get', isGet=True, **data)
+
+    def put(self, **data):
+        return self.__urlRequest('put', **data)
+
+    def post(self, **data):
+        return self.__urlRequest('post', **data)
+
+    def delete(self, **data):
+        return self.__urlRequest('delete', **data)
+
+class HandlerLiason(object):
+    def __init__(self, baseUrl, *args, **kwargs):
+        self.baseUrl = baseUrl
+        self.handler = DbConn(baseUrl)
+
+    def postConn(self, **data):
+        return self.handler.post(**data)
+
+    def deleteConn(self, **data):
+        return self.handler.delete(**data)
+
+    def putConn(self, **data):
+        return self.handler.put(**data)
+
+    def getConn(self, **data):
+        return self.handler.get(**data)
---
+++ restDriver.py
@@ -0,0 +1,104 @@
+#!/usr/bin/env python3
+# Author: Emmanuel Odeke <odeke@ualberta.ca>
+
+import os
+import sys
+import json
+from optparse import OptionParser
+
+from ___dbLiason import HandlerLiason
+from ___fileOnCloudHandler import FileOnCloudHandler
+
+isCallable = lambda a: hasattr(a, '__call__')
+getDefaultAuthor = lambda: os.environ.get('USER', 'Anonymous')
+
+def produceAndParse(func, **dataIn):
+    dbCheck = func(**dataIn)
+    if  hasattr(dbCheck, 'get') and isCallable(dbCheck.get):
+        response = dbCheck.get('value', None)
+        try:
+            outValue = json.loads(response.decode())
+            outValue['status_code'] = dbCheck.get('status_code', 200)
+            return outValue
+        except Exception as e:
+            return dict(reason=str(e), status_code=200)
+    else:
+        return dbCheck
+
+class RestDriver:
+    def __init__(self, ip, port, checkSumAlgoName='sha1'):
+        self.__checkSumAlgoName = checkSumAlgoName or 'sha1'
+        self.__baseUrl = '{i}:{p}'.format(i=ip.strip('/'), p=port.strip('/'))
+        self.__jobLiason = HandlerLiason(self.__baseUrl + '/jobTable/jobHandler')
+        self.__workerLiason = HandlerLiason(self.__baseUrl + '/jobTable/workerHandler')
+        self.__externNameToLiasonMap = dict(Job=self.__jobLiason, Worker=self.__workerLiason)
+
+        self.__fCloudHandler =  FileOnCloudHandler(
+            self.__baseUrl, self.__checkSumAlgoName
+        )
+
+        # Creating functions and table handlers
+        self.newJob = self.__createLiasableFunc('Job', 'postConn')
+        self.newWorker = self.__createLiasableFunc('Worker', 'postConn')
+
+        self.getJobs = self.__createLiasableFunc('Job', 'getConn')
+        self.getWorkers= self.__createLiasableFunc('Worker', 'getConn')
+
+        self.updateJobs = self.__createLiasableFunc('Job', 'putConn')
+        self.updateWorkers = self.__createLiasableFunc('Worker', 'putConn')
+
+        self.deleteJobs = self.__createLiasableFunc('Job', 'deleteConn')
+        self.deleteWorkers = self.__createLiasableFunc('Worker', 'deleteConn')
+
+    def __createLiasableFunc(self, key, methodKey, **attrs):
+        liason = self.__externNameToLiasonMap.get(key, None)
+        method = getattr(liason, methodKey, None)
+        if method is None:
+            method = lambda **aux: aux
+        return method
+
+    def uploadFile(self, srcPath, **attrs):
+        return self.__fCloudHandler.uploadFileByPath(srcPath, **attrs)
+
+    def downloadFile(self, key, **attrs):
+        return self.__fCloudHandler.downloadFileToDisk('documents/' + key)
+
+    def deleteFile(self, **attrs):
+        return self.__fCloudHandler.deleteFileOnCloud(**attrs)
+
+    def updateFile(self, key, **attrs):
+        return self.__fCloudHandler.updateFileByPath(key, **attrs)
+
+    def getCloudFilesManifest(self, **queryParams):
+        return self.__fCloudHandler.getParsedManifest(queryParams)
+
+def cliParser():
+    parser = OptionParser()
+    parser.add_option('-i', '--ip', default='http://127.0.0.1', help='Port server is on', dest='ip')
+    parser.add_option('-p', '--port', default='8000', help='IP address db connects to', dest='port')
+
+    return parser.parse_args()
+
+def main():
+    args, options = cliParser()
+    restDriver = RestDriver(args.ip, args.port)
+
+    print(restDriver.newWorker(name='SpeedBuggy', purpose='Individual Checks'))
+    print(restDriver.newJob(
+        message='http://google.ca/someday', author=getDefaultAuthor(), assignedWorker_id=1
+    ))
+
+    print(restDriver.updateWorkers(
+        queryParams=dict(name='SpeedBuggy'), updatesBody=dict(purpose='Crawling Speed checks')
+    ))
+
+    print('Updating', restDriver.updateJobs(
+        queryParams=dict(message='TheStrokes-Someday'), updatesBody=dict(status='finished')
+    ))
+
+    print(restDriver.getCloudFilesManifest(select='size,checkSum'))
+    # print(restDriver.deleteJobs())
+    # print(restDriver.deleteWorkers())
+
+if __name__ == '__main__':
+    main()
