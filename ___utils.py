fbb7c6c6622dc1b1f583448545e844c6adbc6a9b
Emmanuel Odeke <odeke@ualberta.ca>
2014-09-22 11:40:21 -0600

    HMAC signature utils

--- ___utils.py
+++ ___utils.py
@@ -2,6 +2,8 @@
 
 import os
 import re
+import hmac
+import hashlib
 
 isCallable = lambda a: hasattr(a, '__call__')
 isCallableAttr = lambda obj, attrStr: isCallable(getattr(obj, attrStr, None))
@@ -10,6 +12,12 @@ getDefaultAuthor = lambda: os.environ.get('USER', 'Anonymous')
 getDefaultUserName = getDefaultAuthor
 docStartRegCompile = re.compile('^documents', re.UNICODE)
 
+def getHMACSignature(key, msg, digestmod=hashlib.sha256):
+    return hmac.HMAC(key, msg, digestmod)
+
+def getHMACHexDigest(*args, **kwargs):
+    return getHMACSignature(*args, **kwargs).hexdigest()
+
 # Custom Exceptions
 
 class UnReadableStreamException(Exception):
@@ -17,4 +25,3 @@ class UnReadableStreamException(Exception):
 
 class UnWriteableStreamException(Exception):
     pass
-
